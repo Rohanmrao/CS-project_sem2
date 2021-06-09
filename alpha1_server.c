fc03792ef0c3accabc8c5bf46b5bf83008e0f764
ROHAN MAHESH RAO <70153723+Rohanmrao@users.noreply.github.com>
2021-06-09 13:26:17 +0530

    Now shows the list of places
    when ticket_book() is invoked

--- alpha1_server.c
+++ alpha1_server.c
@@ -2,13 +2,35 @@
 #include <string.h>
 #include "alpha1_server.h"
 
-// creating all the functions that will land 
+// creating all the functions that will land on the opening page  
 
 void ticket_book()
 {
     printf("Welcome to <name> !");
     printf("Enter start and stop destination from the given options:\n");
     
-    int i;
+    FILE *display_places_ptr; char display_options
     
+    display_places_ptr = fopen("places.txt",r);
+    while(1)
+    {
+      display_options = fgetc(display_places_ptr);
+        if(feof(display_places_ptr))
+        {
+            break;
+        }
+        
+        printf("%c\n",display_options);
+    }
+    
+    printf("\n");
+    printf("Enter starting station: ");
+    char startpoint[40];
+    scanf("%[^\n]%",&startpoint);
+    
+    printf("Enter the destination station: ");       // a mechanism to check if the user has entered something from the options itself and not randomly
+    char endpoint[40];
+    scanf("%[^\n]%",&endpoint);
+    
+        
 }

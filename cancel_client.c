365fc808a743f6141b21356dbb797588a4b13379
roshansrihariofficial <85618770+roshansrihariofficial@users.noreply.github.com>
2021-06-13 19:22:29 +0530

    Add files via upload

---
+++ cancel_client.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	int a,i,otp,otp_enter;
+	long double ph;
+
+	srand(time(0));
+	printf("TO PROCEED FOR CANCELLATION, PRESS '1'\n");
+	scanf("%d",&a);
+	if(a==1)
+	{
+		printf("ENTER PHONE NUMBER: ");
+		scanf("%ld",&ph);
+		otp = rand();
+		printf("OTP: %d\n",otp);
+		printf("ENTER YOUR OTP: ");
+		scanf("%d",&otp_enter);
+		if(otp_enter==otp)
+		{
+			printf("TICKETS HAVE BEEN CANCELED!\n");
+		}
+		else
+		{
+			printf("INVALID OTP\n");
+		}
+	}
+	printf("THANK YOU\n");
+	return 0;
+}
+
+
+		
+			
+		
+		
+		 
+		
+
+	
+	

8ff2dbcf0abfb4340db10b95e0a27e1519e60e77
roshansrihariofficial <85618770+roshansrihariofficial@users.noreply.github.com>
2021-06-13 19:00:13 +0530

    Update login.c

--- login.c
+++ login.c
@@ -1,35 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>  
 #include<time.h>
-int random_num();
+void random_num();
 int main()
 {
-    char phone_no;
+    char phone_no[20];
     printf("\n  =======================  LOGIN FORM  =======================\n  ");
-    printf(" \n                       ENTER YOUR PHONE NUMBER:-");
+    printf(" \n                       ENTER YOUR PHONE NUMBER: ");
     scanf("%s",&phone_no);
     random_num();
     printf("");
 }
-int random_num()
+
+
+void random_num()
 {
     int a,otp,i;
     srand(time(NULL));
-    printf("                       ONE TIME PASSWORD is :-");
+    printf("                       ONE TIME PASSWORD is : ");
     a=rand();
     printf("%d\n",a);
-    printf(" \n                       ENTER ONE TIME PASSWORD:-");
-    scanf("%d",otp);
+    printf(" \n                       ENTER ONE TIME PASSWORD: ");
+    scanf("%d",&otp);
     if(a==otp)
     {
         printf("  \n\n\n       WELCOME TO OUR SYSTEM !! YOUR LOGIN IS SUCCESSFUL");
-	    printf("\n\n\n\t\t\t\tPress any key to continue...");
+	printf("\n\n\n\t\t\t\tPress any key to continue...");
     }
     else
     {
         printf("\n        SORRY !!!!  LOGIN IS UNSUCESSFUL");
-		a++;
-		system("cls");
+	a++;
+	system("cls");
         i++;
     }
 }

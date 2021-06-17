5c4aacb4cc4a928011c74dd2ef939bdf4e166ab1
ROHAN MAHESH RAO <70153723+Rohanmrao@users.noreply.github.com>
2021-06-17 13:54:38 +0530

    Update alpha1_server.h

--- alpha1_server.h
+++ alpha1_server.h
@@ -9,4 +9,5 @@ void cancel_end(); // Roshan
 
 void random_num(); //Neelan
 
-void final_ticket(int s, int t); //Poornachandra
+void final_ticket(int s, int t); 
+void disp(char name);     //Poornachandra

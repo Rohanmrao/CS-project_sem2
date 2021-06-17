67a1266f55de7a75750699853536bcdb01184043
ROHAN MAHESH RAO <70153723+Rohanmrao@users.noreply.github.com>
2021-06-17 13:52:40 +0530

    Update alpha1_server.c

--- alpha1_server.c
+++ alpha1_server.c
@@ -196,9 +196,58 @@ int random_num()
     }    
     else 
     {
-        printf("\n              SORRY !!!!  LOGIN IS UNSUCESSFULL");
-        
-        
+        printf("\n              SORRY !!!!  LOGIN IS UNSUCCESSFULL");   
     }
+}
+void final_ticket(int s, int t)
+{
+	int dm = rates_dist();
+   
+  // reservation multipliers: 1- rs 25 | 2 - rs 20 |3 - rs 15 |4 - rs 10
+  if (s>=1 && s<=4)
+  {
+    if(s==1)
+    {
+      printf("coach class: AC CHAIR");
+      rm= 25;
+    }   
+    if(s==2)
+    {
+      printf("coach class: AC SLEEPER");
+      rm=20;
+    }  
+    if(s==3)
+    {
+      printf("coach class: SLEEPER");
+      rm= 15;
+    }   
+    else
+    {
+      printf("coach class:  CHAIR");
+      rm=10;
+    }     
+  }
+  else
+  {
+    printf("error");
+  }
+  
+  //final price = distance_multiplier * number of tickets * reservation multiplier
+  // reservation multipliers: 1- rs 25 | 2 - rs 20 |3 - rs 15 |4 - rs 10
+  int fp ,dm;
+  fp = dm*t*rm;
+  printf("total cost : %f",fp);
+
+}
+
+void disp(char name)
+{ 
+  printf("name : %s",name) ;
+  printf("mobile phone: ");
+  printf("booking status: BOOKED");
+  printf ("name of the train: ");
+  printf ("depature time:");
+  printf ("arrival  time:");
+  printf("car number:");
 }
 

2fa725cfce4e0b3bdf83566d7e3ecf7314941a01
ROHAN MAHESH RAO <70153723+Rohanmrao@users.noreply.github.com>
2021-06-17 08:33:51 +0530

    added the workflow of our final code, check it out

--- alpha1_client.c
+++ alpha1_client.c
@@ -21,10 +21,30 @@ int main()
 	printf("\n=================================\n");
 	printf("    TRAIN RESERVATION SYSTEM - LOGIN/CANCEL PAGE");
 	printf("\n=================================");
+	// neelan's function will come here
+	// roshan's cancel function comes here
 
 	printf(">> View All Available Trains");
 	printf("\n------------------------");
-    train_disp();
+    	train_disp();
+	places_disp();
+	
+	int distance_val = 0;
+	printf("Enter the Sl.no of the start point: ");
+	int p1 = 0;
+	scanf("%d",&p1);
+	printf("Enter the Sl.no of the end point: ");
+	int p2 = 0;
+	scanf("%d",&p2);
+	
+	enterplace(p1,p2,distance_val);
+	rates_dist(); // the first rate multiplier is generated here
+	
+	// Poorna's bio data and ticket book function comes here
+	
+	
+	
+	
 	
 	return(0);
 }

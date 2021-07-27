aaf9a33d22e398938d5cccd96f8e802aeefdaafb
roshansrihariofficial <85618770+roshansrihariofficial@users.noreply.github.com>
2021-07-27 10:06:33 +0530

    altered names of trains

--- alpha1_server.c
+++ alpha1_server.c
@@ -17,8 +17,8 @@ void train_disp()
                             ,"SiddagangaIntercity","VishwamanavaExpress","ChamundiExpress"
                             ,"KaveriExpress","BasavaExpress","GolGumbazExpress","TippuSuperfastExpress"    
                             ,"DurontoExpress","GaribRathExpress","MalgudiExpress","GomateshwaraExpress"
-                           ,"KarnatakaSamparkKrantiExpress","AiravathaExpress"
-						   ,"Speed1Express","Speed2Express","Speed3Express"
+                           ,"KarnatakaSamparkKrantiExpress","AmaravatiExpress"
+						   ,"PanchagangaExpress","NightQueenPassenger","PrashantiNilayamExpress"
                         };   // database of trains 
 						
 		int time_hr[25] = {1,2,3,4,5,6,7,8,9,10,11,12,11,10,9,8,7,6,5,4,3,2,1,4,5};

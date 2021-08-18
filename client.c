99c093f5b2a5822a1a4ef3497b114ff339e8eca2
Rohan Mahesh Rao <rohanmaheshrao10@gmail.com>
2021-08-18 11:09:43 +0530

    push2

--- client.c
+++ client.c
@@ -6,20 +6,14 @@
 
 
 //***********************************GLOBALS***********************************************
-	char name[50], cancel_choice[1];	// name : stores the name of the user 			
-	int age ,rm;                        // rm :resvervation multiplier
-	int ser,tic;                        //ser : serial number of the coach class ; tic: number of tickets to be booked
-	int train_choice, choice_flag = 1;  // train_choice: displays the train alloted 
+	char name[50], cancel_choice[1];				
+	int age ,rm;
+	int ser,tic;
+	int train_choice, choice_flag = 1;
 
-	int p1 = 0;int p2 = 0;              //p1: starting station serial number ; p2 : destination serial number 
-    
-	
-    int dd, mm, yy;                     // day, month, year 
-	int flag = 0,cout,a;                //flag and cout are counting variables , a stores value of check function
+	int p1 = 0;int p2 = 0;
 
-
-	
-	int payup,dm;                       //payup:stores final cost of ticket  ; dm : distance mutliplier
+	int payup,dm;
 
 	double mobileNum; double mobilenum_fordisplay;
 
@@ -28,7 +22,7 @@
 	char *places[50] = {"BangaloreCity", "MumbaiCST", "NewDelhi", "Bijapur", "Kolhapur", "Mysore", "Badami", "ChennaiCentral", "Secunderabad", "Hubli", "Hospet", "Jaipur", "Lucknow", "Solapur", "Mangalore"};
 //*******************************************************************************************
 
-int main()
+int main()                                            //Main Function
 
 { 
 	printf("\t\t=================================================\n");
@@ -44,66 +38,55 @@ int main()
 	printf("\t\t|                                               |\n");
 	printf("\t\t=================================================\n\n\n");
 	
-	printf("=================================\n");
+	printf("========================================================\n");
 	printf("    TRAIN RESERVATION SYSTEM - LOGIN/CANCEL PAGE\n");
-	printf("=================================\n");
-	printf("=======================  LOGIN FORM =======================\n  ");
-    printf("ENTER YOUR PHONE NUMBER: ");
-	scanf("%lf",&mobileNum);                 // inputing the phone number
+	printf("========================================================\n");
+	printf("\n");
+	printf("======================= LOGIN FORM =====================\n  ");
+	printf("\n");
+    printf("ENTER YOUR PHONE NUMBER: ");              //User enters the mobile number
+	scanf("%lf",&mobileNum);
 	getchar();
 
-	mobileNumber(mobileNum);         
-	random_num(mobileNum);
-	getchar();
+	mobileNumber(mobileNum);                         //mobilenumber() is called to verify whether the entered number is 10 digit or not 
+	random_num(mobileNum);							//random_num() is called to generate the random four digit number
+	//getchar();
 
+	printf("\n");
 	printf("Do you want to cancel an existing ticket ? (y/n): ");
 	scanf("%s",cancel_choice);getchar();
 
+
 	if(strcmp(cancel_choice,"y") == 0)
 	{
-		cancel_front(mobileNum);
+		cancel_front(mobileNum);                    //cancel_front() is called if the user wants to cancel the previously booked tickets
 	}
-	
+	printf("\n");
 	places_disp();
-	printf("\n-------------------------\n");
+	printf("\n");
+	printf("-------------------------\n");
 	
-	
-	printf("Enter the Sl.no of the start point: ");
+	printf("\n");
+	printf("Enter the Sl.no of the start point: ");  // User enters the starting point
 	scanf("%d",&p1); getchar();
 	strcpy(loc.origin, places[p1 - 1]);
-	printf("Enter the Sl.no of the end point: ");
+	printf("\n");
+	printf("Enter the Sl.no of the end point: ");    // User enters the end point 
 	scanf("%d",&p2);getchar();
 	strcpy(loc.destination, places[p2 - 1]);
-	dm = enterplace(p1,p2); // distance  multiplier generated here 
+	printf("\n");
+	dm = enterplace(p1,p2);                          // enterplace() is called to verify whether the entered places are valid or not  
 
-
-    
 	
-	printf(" \n ---------------------DATES AVAILABLE WITHIN 1 YEAR PERIOD  -------------------------\n ----------NOTE : booking date and travelling dates must have  minimum of 1 day gap------------- \n ");
-
-	printf("Enter the  booking date  (DD/MM/YYYY) : ");         
-	scanf("%d/%d/%d",&dd,&mm,&yy);getchar();
-	a=check(dd,mm ,yy);
-	while(flag==0)  // executed only if the entered date is invalid 
-    {
-        printf("Please enter a valid date (DD/MM/YYYY format): ");
-        scanf("%d/%d/%d",&dd,&mm,&yy);
-		fflush(stdin);
-        check(dd,mm ,yy); // re-checking if the entered date is valid 
-    }    
-	fflush(stdin);
-	
-	
-
-
-	train_disp();
+	train_disp();                                   // train_disp() displays the trains availabel
 	printf("\n");
 	printf("You have been allotted train number %d... \n",choice_flag);
+	printf("\n");
 	printf("Type 1 to accept... 2 to reject :");
 	scanf("%d",&train_choice); getchar();
 	choice_flag=2;
 
-	if(train_choice == 2 && choice_flag>=2)
+	if(train_choice == 2 && choice_flag>=2)        // Loop for realloting the train
 	{
 		printf("------------------------------\n");
 		printf("Reallotting...\n");
@@ -133,63 +116,47 @@ int main()
 		choice_flag=5;
 	}
 
-	
-	// accepting user details
-	
+	printf("\n");
 	printf("-----------------------------------\n");
-  	printf("Enter you name: ");
+	printf("\n");
+  	printf("NAME: ");                              // User enters the details,the required type of seat and number of seats
 	fflush(stdin);
   	scanf("%s",&name);getchar();
-  	printf("Enter your age: ");
+  	printf("Age: ");
 	fflush(stdin);
   	scanf("%d",&age);getchar();
-  	
-	printf("Coach Class: 1.AC CHAIR  | 2.AC SLEEPER | 3.CHAIR | 4.SLEEPER \n"); // selection of coach class 
+  	printf("Coach class: 1.AC CHAIR  | 2.AC SLEEPER | 3.CHAIR | 4.SLEEPER \n");
   	printf("Select coach class by entering the number next to it: ");
   	scanf("%d",&ser);getchar();
-  	printf("Number of Tickets:");
+  	printf("Number of tickets:");
   	scanf("%d",&tic);getchar();
-	payup = final_ticket(ser,tic,dm); // total amount of the ticket
-  	
-	while(cout==0)
-	{
-       printf("Select coach class by entering the number next to it: ");
-  	   scanf("%d",&ser);getchar();
-  	   printf("Number of Tickets:");
-  	   scanf("%d",&tic);getchar();
   
-	   payup = final_ticket(ser,tic,dm);    
-	}
-	
-	
-	
-	fflush(stdin);
-	
-	// bill displaying data based on info collected from user
-	printf("\n---------- BILL ------------\n");
-	printf("Name:");
+	payup = final_ticket(ser,tic,dm);              // final_ticket() is called to calculate the final amount 
+  	
+	printf("\n");
+	printf("---------- BILL ------------\n");
+	printf("\n");
 	puts(name);
 	printf("Age : %d\n",age);
 	mobilenum_fordisplay = mobileNumber(mobileNum);
-  	printf("Ticket(s) booked for : %d/%d/%d\n",dd,mm,yy);
-	printf("Mobile Fhone: %.0f\n",mobilenum_fordisplay);
+  	printf("mobile phone: %.0f\n",mobilenum_fordisplay);
 	printf("From: %s\n", loc.origin);
 	printf("To: %s\n", loc.destination);
-  	printf("Booking Status: BOOKED\n");
-	printf("Total Amount: %d\n",payup);
-	booked_on();
-    printf("\n-------------------------------------------------");
-
-
-
-	printf("\n Do you want to cancel the ticket you just booked ? (y/n): ");
+  	printf("booking status: BOOKED\n");
+	printf("Total amount: %d\n",payup);
+	
+	printf("Do you want to cancel the ticket you just booked ? (y/n): ");   // User is asked for final Confirmation of tickek to be booked 
 	scanf("%s",cancel_choice);getchar();
 
 	if(strcmp(cancel_choice,"y") == 0)
 	{
 		cancel_end(mobileNum);
 	}
-	
+	else
+	{
+		printf("\n");
+		printf("Thank You");
+	}	
 	
 	return(0);
 }
--- server.c
+++ server.c
@@ -4,9 +4,9 @@
 #include <time.h>
 #include <stdlib.h>
 
-// creating all the functions that will land on the opening page  
+// Defnition for all the functions that will land on the opening page  
 
-double mobileNumber(double mb) //Function definition
+double mobileNumber(double mb)                                       //Checks whether the mobilenumber entered is 10 digit or not
 {
     if (mb < 1000000000 || mb > 9999999999)
     {
@@ -16,22 +16,24 @@ double mobileNumber(double mb) //Function definition
     return mb;
 }
 
-void random_num(double mob)
+void random_num(double mob)                                         // Generates the 4 digit Random number between 1000 and 9999
 {
     int i = 0;
     
     printf("\n");
-    printf("                       ONE TIME PASSWORD is : ");
+    printf("                       ONE TIME PASSWORD is  : ");
     int otp, trials = 0; 
 	int a = otp_gen(1000,9999);
     printf("%d\n",a);
-    printf("                        ENTER ONE TIME PASSWORD: ");
+	printf("\n");
+    printf("                       ENTER ONE TIME PASSWORD: ");
     scanf("%d",&otp);
 	getchar();
     
     if(a==otp)
     {
-        printf("      WELCOME TO OUR SYSTEM !! YOUR LOGIN IS SUCCESSFUL\n");
+        printf("\n");
+		printf("      WELCOME TO OUR SYSTEM !! YOUR LOGIN IS SUCCESSFUL\n");
 	 
         FILE *fp;
 	    fp=fopen("logindetails.txt","w");
@@ -45,18 +47,19 @@ void random_num(double mob)
     }
 }
 
-void places_disp()
+void places_disp()                                       // This is the function that will show all the available places 
 {
-	// this is the function that will show all the available places 
-
+	
 	printf("Here are the available stations:\n");
+	
+
 	FILE* places_disp;
     int display;
  
     
-    places_disp = fopen("places.txt", "r");
+    places_disp = fopen("places.txt", "r");            // Loop to extract all characters
  
-    // loop to extract all characters
+    
     while (1) {
         // reading file
         display = fgetc(places_disp);
@@ -72,10 +75,10 @@ void places_disp()
 }
 
 
-int enterplace(int p1,int p2)
+int enterplace(int p1,int p2)                          //Function to take the input for two listed stations
 {
 	int distance_val = 0, dist_multiplier, flag = 0;
-	if(p2 > p1){
+	if(p2 > p1){                                      //Loop to verify whether entered value for stations are valid or not
 		distance_val = p2-p1;
 		flag++;
 	}
@@ -108,21 +111,21 @@ int enterplace(int p1,int p2)
 	return dist_multiplier ; 
 }
 
-int flag;//  a counting variable
+int flag;                                               //  a counting variable
 
-int check(int dd,int mm ,int yy)// validating the entered dates assuming all months have only 30 days
+int check(int dd,int mm ,int yy)                       // validating the entered dates assuming all months have only 30 days
 {     
 
-    time_t t;                // time_t  is a type suitable for storing calendar time
+    time_t t;                                         // time_t  is a type suitable for storing calendar time
     t = time(NULL);
-    struct tm tm = *localtime(&t); //accessing local time
+    struct tm tm = *localtime(&t);                   //accessing local time
     flag=0;
 
-   //tm.tm_year+1900 : current year  ||  tm.tm_mon+1 : current month ||   tm.tm_mday: current day of the month
+                                                    //tm.tm_year+1900 : current year  ||  tm.tm_mon+1 : current month ||   tm.tm_mday: current day of the month
 
-    if(yy==(tm.tm_year+1900)) // checking if the user entered the current year
+    if(yy==(tm.tm_year+1900))                      // checking if the user entered the current year
     {
-       if(mm==(tm.tm_mon+1)) // checking if the enter month matches the current the month number
+       if(mm==(tm.tm_mon+1))                      // checking if the enter month matches the current the month number
        {
            if(dd>tm.tm_mday && dd<=30)
             {
@@ -131,7 +134,7 @@ int check(int dd,int mm ,int yy)// validating the entered dates assuming all mon
               
         }
 
-       if (mm>(tm.tm_mon+1) && mm<=12)// since the year is same , month number accepted must be greater than current month number and less than 12
+       if (mm>(tm.tm_mon+1) && mm<=12)          // since the year is same , month number accepted must be greater than current month number and less than 12
         {
             if(dd>=1 && dd<=30)
             {
@@ -141,20 +144,20 @@ int check(int dd,int mm ,int yy)// validating the entered dates assuming all mon
     
     }
 
-    if (yy==(tm.tm_year+1901))// checking if the entered  year is the next year and the following is executed when user enters the next year
+    if (yy==(tm.tm_year+1901))                 // checking if the entered  year is the next year and the following is executed when user enters the next year
     {
-       if(mm==(tm.tm_mon+1))// checking if the month number for the next year matches the current month number 
+       if(mm==(tm.tm_mon+1))                  // checking if the month number for the next year matches the current month number 
        {
-           if(dd<=tm.tm_mday && dd>=1)//since tickets are availble for 1 year period , the  dates accepted will range from (1 to current date number)
+           if(dd<=tm.tm_mday && dd>=1)       //since tickets are availble for 1 year period , the  dates accepted will range from (1 to current date number)
               {
                  return  flag++;
               }
               
         }
 
-        if (mm<(tm.tm_mon+1) && mm<=12)// user enterd month must be less than the current month number to satisfy 1 year booking period
+        if (mm<(tm.tm_mon+1) && mm<=12)     // user enterd month must be less than the current month number to satisfy 1 year booking period
         {
-            if(dd>=1 && dd<=30)// all date numbers are vaild 
+            if(dd>=1 && dd<=30)             // all date numbers are vaild 
             {
                return flag++;
             }
@@ -165,7 +168,7 @@ int check(int dd,int mm ,int yy)// validating the entered dates assuming all mon
 
 
 
-void train_disp() // displays train details
+void train_disp()                           // Displays the availabel train details
 {
     printf("Here are the available trains to your destination...\n");
 	printf("___(The deprature timings are mentioned below the train names)___ \n");
@@ -178,7 +181,7 @@ void train_disp() // displays train details
                             ,"DurontoExpress","GaribRathExpress","MalgudiExpress","GomateshwaraExpress"
                            ,"SamparkKrantiExpress","AmaravatiExpress"
 						   ,"PanchagangaExpress","NightQueenPassenger","HumsafarExpress"
-                        };   // database of trains 
+                        };                 // Database of trains 
 						
 		int time_hr[25] = {1,2,3,4,5,6,7,8,9,10,11,12,11,10,9,8,7,6,5,4,3,2,1,4,5};
 		int time_min[25] = {10,15,20,30,35,40,45,50,40,35,30,25,20,15,10,10,15,20,25,30,30,30,30,45,45};   // database of times 
@@ -188,9 +191,7 @@ void train_disp() // displays train details
 	
         for(int i = 0; i < 5; i++)
         {
-                        
-                        
-
+                   
             ind1 = ((rand() % (u - l + 1)) + l);
 			int hr = time_hr[ind1];
 			int min = time_min[ind1];
@@ -205,18 +206,17 @@ void train_disp() // displays train details
                 printf(" %d:%d PM\n",hr,min);
             }
         }
-
 }
 
 
 
-int cout;// a counting variable
+int cout;                             //A counting variable
 
-int final_ticket(int s, int t,int distmult)// calculates the final ticket cost based on vaild input
+int final_ticket(int s, int t,int distmult)      //Calculates the final ticket cost based on vaild input
 {	
 	
     cout =0;
-  // reservation multipliers: 1- rs 25 | 2 - rs 20 |3 - rs 15 |4 - rs 10
+                                    //Reservation multipliers: 1- rs 25 | 2 - rs 20 |3 - rs 15 |4 - rs 10
   if (s>=1 && s<=4)
   { 
 	cout++;
@@ -237,40 +237,41 @@ int final_ticket(int s, int t,int distmult)// calculates the final ticket cost b
       return (10*s*t*distmult);
     }     
   } 
-  // formula used to calculate the cost : final price = distance_multiplier * number of tickets * reservation multiplier 
-  // reservation multipliers: 1- rs 25 | 2 - rs 20 |3 - rs 15 |4 - rs 10
+                                    // formula used to calculate the cost : final price = distance_multiplier * number of tickets * reservation multiplier 
+                                   // reservation multipliers: 1- rs 25 | 2 - rs 20 |3 - rs 15 |4 - rs 10
   
 }
 
-int booked_on()// displays real time at which the ticket was booked
+int booked_on()                   // displays real time at which the ticket was booked
 {
  
     time_t t;
     t = time(NULL);
     struct tm tm = *localtime(&t); 
-    printf("this tickect was booked on: %d-%d-%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
+    printf("This tickect was booked on: %d-%d-%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year+1900);
     
 }
 
 
 
-void cancel_end(double mobileNumber)
+void cancel_end(double mobileNumber)  // Function to cancel a ticket at the end of the transaction
 {
 	int a, otp, otp_enter;
 
-	printf("	TO PROCEED TO CANCELLATION, PRESS '1':  \n");    //USER CONFIRMS WHETHER HE WANTS TO PROCEED TO CANCELLATION.
+	printf("	TO PROCEED TO CANCELLATION, PRESS '1':  \n");    // USER CONFIRMS WHETHER HE WANTS TO PROCEED TO CANCELLATION.
 	scanf("%d", &a);
 	if(a == 1)
 	{
 		int l = 1000, u = 9999, otp, trials = 0;
 
 		//srand(time(0)); 
-		otp = otp_gen(l,u);  //ENSURING THAT THE RANDOM OTP GENERATED WILL CONSIST OF 4 DIGITS ONLY.
-		printf("	OTP: %d\n", otp);
-		//printf("\n");
-		printf("	ENTER YOUR OTP: ");
+		otp = otp_gen(l,u);          //ENSURING THAT THE RANDOM OTP GENERATED WILL CONSIST OF 4 DIGITS ONLY.
+		printf("	ONE TIME PASSWORD  is   : %d\n", otp);
+		printf("\n");
+		printf("	ENTer ONE TIME PASSWORD : ");
 		scanf("%d", &otp_enter);
-		//WE SHALL LIMIT THE NUMBER OF TRIALS OF OTP ENTRY TO 4.
+		printf("\n");
+		                            //WE SHALL LIMIT THE NUMBER OF TRIALS OF OTP ENTRY TO 4.
 		while((otp_enter != otp) && (trials <= 2))
 		{
 			printf("	INVALID OTP! PLEASE TRY AGAIN \n");
@@ -279,7 +280,7 @@ void cancel_end(double mobileNumber)
 			scanf("%d", &otp_enter);
 			trials++;
 		}
-		//CHECKING IF THE NUMBER ENTERED MATCHES THE GENERATED OTP
+		                           //CHECKING IF THE NUMBER ENTERED MATCHES THE GENERATED OTP
 		if(otp_enter == otp)
 		{
 			printf("\n");
@@ -299,7 +300,7 @@ void cancel_end(double mobileNumber)
 
 
 
-int otp_gen(int l,int u)
+int otp_gen(int l,int u) // Generates the  One Time Password
 {
 	srand(time(0));
 	return ((rand() % (u - l + 1)) + l);
@@ -307,29 +308,29 @@ int otp_gen(int l,int u)
 }
 
 
-void cancel_front(double mobileNum)
+void cancel_front(double mobileNum)  // Function to cancel the already booked tickets 
 {
 	FILE *fp; 
 	
-	//ATTEMPT TO OPEN THE TEXT FILE
+	                                //ATTEMPT TO OPEN THE TEXT FILE
 	fp = fopen("logindetails.txt","r");
-	//CHECKING IF THE FILE EXISTS
+	                               //CHECKING IF THE FILE EXISTS
 	if(fp == NULL)
 	{
 		printf("	FILE DOES NOT EXIST! \n");
 	}
 
-	//OPENING THE FILE AND PROCEEDING IF FILE EXISTS
+	                              //OPENING THE FILE AND PROCEEDING IF FILE EXISTS
 	else
 	{
 		double ph;
 
 		printf("PLEASE ENTER YOUR PHONE NUMBER:  \n");   //PROMPTING USER TO ENTER THE PHONE NUMBER
 		scanf("%lf", &ph);
-		if(ph==mobileNum)   //CHECKING IF THE ENTERED PHONE NUMBER MATCHES THAT PRESENT IN THE TEXT FILE
+		if(ph==mobileNum)        //CHECKING IF THE ENTERED PHONE NUMBER MATCHES THAT PRESENT IN THE TEXT FILE
 		{
 
-			//GENERATING AN OTP
+			                    //GENERATING AN OTP
 			int otp, otp_enter;
 			int trials = 0;
 
@@ -338,11 +339,11 @@ void cancel_front(double mobileNum)
 			printf("	OTP: %d\n", otp);
 			printf("\n");
 
-			//PROMPT FOR USER TO ENTER THE OTP
+			                         //PROMPT FOR USER TO ENTER THE OTP
 			printf("	ENTER YOUR OTP: ");
 			scanf("%d", &otp_enter);
 
-			//WE SHALL LIMIT THE NUMBER OF TRIALS OF OTP ENTRY TO 4.
+			                        //WE SHALL LIMIT THE NUMBER OF TRIALS OF OTP ENTRY TO 4.
 			while((otp_enter != otp) && (trials <= 2))
 			{
 				printf("	INVALID OTP! PLEASE TRY AGAIN \n");
@@ -352,7 +353,7 @@ void cancel_front(double mobileNum)
 				trials++;
 			}
 
-			//CHECKING IF THE NUMBER ENTERED MATCHES THE GENERATED OTP
+			                       //CHECKING IF THE NUMBER ENTERED MATCHES THE GENERATED OTP
 			if(otp_enter == otp)
 			{
 				printf("\n");
@@ -366,7 +367,7 @@ void cancel_front(double mobileNum)
 		
 		}
 
-		else    //IF PHONE NUMBER ENTERED DOES NOT MATCH THAT PRESENT IN THE TEXT FILE
+		else                       //IF PHONE NUMBER ENTERED DOES NOT MATCH THAT PRESENT IN THE TEXT FILE
 		{
 			printf("\n");
 			printf("	INVALID PHONE NUMBER! \n");
